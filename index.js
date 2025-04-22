bd6bf4bb2c46d7db205125eb2abbaf58a5e54648
WalidurTanjim <mohammadtanjim91@gmail.com>
2025-04-22 21:29:46 +0600

    Start learn TS

---
+++ index.js
@@ -0,0 +1,7 @@
+"use strict";
+let fName = "Walidur";
+let lName = "Tanjim";
+let fullName = "Walidur Tanjim";
+let age = 24;
+let isMarried = false;
+console.log(fName, lName, fullName, age, isMarried);
---
+++ index.ts
@@ -0,0 +1,6 @@
+let fName: string = "Walidur";
+let lName: string = "Tanjim";
+let fullName: string = "Walidur Tanjim";
+let age: number = 24;
+let isMarried: boolean = false;
+console.log(fName, lName, fullName, age, isMarried);
