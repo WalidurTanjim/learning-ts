9f5700855a83aafa6b70e5f681516690ee0aeef3
WalidurTanjim <mohammadtanjim91@gmail.com>
2025-04-24 21:28:38 +0600

    Day 04 Practice & Homework done

--- index.js
+++ index.js
@@ -5,7 +5,50 @@
 // let age: number = 24;
 // let isMarried: boolean = false;
 // console.log(fName, lName, fullName, age, isMarried);
-const sumFn = (a, b) => {
-    return a + b;
-};
-console.log(sumFn(5, 10));
+// const sumFn = (a: number, b: number): number => {
+//     return a + b;
+// }
+// console.log(sumFn(5, 10));
+// TODO: Arithmetic Operations:
+// let firstNum:number = 10;
+// let secondNum:number = 20;
+// let result:number = firstNum + secondNum;
+// console.log(result);
+// let nanValue: number = NaN;
+// console.log(typeof nanValue, nanValue);
+// Practice 01:
+let myString = "Hello, TypeScript!";
+// Practice 02:
+let firstName = "Walidur";
+let lastName = "Tanjim";
+let fullName = firstName.concat(lastName);
+// console.log(fullName);
+// Practice 03:
+let sentence = "Alhamdulillah, I'm a Muslim. I wanna be a pure muslim as Prophet Mohammad (S:) & go to Jannat.";
+let sentenceLength = sentence.length;
+// console.log(sentenceLength)
+// Practice 04:
+let myChoice = "Alhamdulillah, I'm a Muslim. I wanna be a pure muslim as Prophet Mohammad (S:) & go to Jannat.";
+let uppercaseText = myChoice.toUpperCase();
+let lowercaseText = myChoice.toLocaleLowerCase();
+// console.log(uppercaseText, lowercaseText);
+// Homework 01:
+let longText = "Alhamdulillah, I'm a Muslim. I wanna be a pure muslim as Prophet Mohammad (S:) & go to Jannat.";
+let shortText = longText.slice(0, 10);
+// console.log(shortText);
+// Homework 02:
+let fName = "Walidur";
+let lName = "Tanjim";
+let areEqual;
+if (fName === lName) {
+    areEqual = "true";
+}
+else {
+    areEqual = "false";
+}
+// console.log(areEqual)
+// Homeword 03:
+let product = "Laptop";
+let price = 45000;
+let productInfo = `Product ${product} is priced at ${price} bangladeshi taka`;
+console.log(productInfo);
--- index.ts
+++ index.ts
@@ -5,8 +5,61 @@
 // let isMarried: boolean = false;
 // console.log(fName, lName, fullName, age, isMarried);
 
-const sumFn = (a: number, b: number): number => {
-    return a + b;
-}
+// const sumFn = (a: number, b: number): number => {
+//     return a + b;
+// }
 
-console.log(sumFn(5, 10));
+// console.log(sumFn(5, 10));
+
+
+
+// TODO: Arithmetic Operations:
+// let firstNum:number = 10;
+// let secondNum:number = 20;
+// let result:number = firstNum + secondNum;
+// console.log(result);
+
+// let nanValue: number = NaN;
+// console.log(typeof nanValue, nanValue);
+
+// Practice 01:
+let myString: string = "Hello, TypeScript!";
+
+// Practice 02:
+let firstName: string = "Walidur";
+let lastName: string = "Tanjim";
+let fullName: string = firstName.concat(lastName);
+// console.log(fullName);
+
+// Practice 03:
+let sentence: string = "Alhamdulillah, I'm a Muslim. I wanna be a pure muslim as Prophet Mohammad (S:) & go to Jannat.";
+let sentenceLength: number = sentence.length;
+// console.log(sentenceLength)
+
+// Practice 04:
+let myChoice: string = "Alhamdulillah, I'm a Muslim. I wanna be a pure muslim as Prophet Mohammad (S:) & go to Jannat.";
+let uppercaseText: string = myChoice.toUpperCase();
+let lowercaseText: string = myChoice.toLocaleLowerCase();
+// console.log(uppercaseText, lowercaseText);
+
+// Homework 01:
+let longText: string = "Alhamdulillah, I'm a Muslim. I wanna be a pure muslim as Prophet Mohammad (S:) & go to Jannat.";
+let shortText: string = longText.slice(0, 10);
+// console.log(shortText);
+
+// Homework 02:
+let fName: string = "Walidur";
+let lName: string = "Tanjim";
+let areEqual: string;
+if(fName === lName){
+    areEqual = "true";
+}else{
+    areEqual = "false"
+}
+// console.log(areEqual)
+
+// Homeword 03:
+let product: string = "Laptop";
+let price: number = 45000;
+let productInfo: string = `Product ${product} is priced at ${price} bangladeshi taka`;
+console.log(productInfo);
