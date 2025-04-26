e24f7277bdddaac0f57131ccba7f177a2c89c499
WalidurTanjim <mohammadtanjim91@gmail.com>
2025-04-26 07:02:09 +0600

    Type Infercence in TS

---
+++ type_inference.js
@@ -0,0 +1,14 @@
+"use strict";
+// Type Inference: Type inference in TS refers to the ability of the TS compiler to automatically determine & assign types to variables, expressions & function return values based on their usage & context in the code.
+const myName = "Walid";
+let myAge = 24;
+let isMarried = false;
+let isStudent = true;
+// Practice 01:
+let message = "Hello, TypeScript!";
+// console.log(message);
+// Prctice 02:
+const calculateArea = (a, b) => {
+    return a * b;
+};
+// console.log(calculateArea(5, 3));
---
+++ type_inference.ts
@@ -0,0 +1,17 @@
+// Type Inference: Type inference in TS refers to the ability of the TS compiler to automatically determine & assign types to variables, expressions & function return values based on their usage & context in the code.
+
+const myName = "Walid";
+let myAge = 24;
+let isMarried = false;
+let isStudent = true;
+
+// Practice 01:
+let message = "Hello, TypeScript!";
+// console.log(message);
+
+// Prctice 02:
+const calculateArea = (a: number, b: number) => {
+    return a * b;
+}
+
+// console.log(calculateArea(5, 3));
