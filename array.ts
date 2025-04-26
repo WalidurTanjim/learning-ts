9266c8ade229a2f67f74c348289c7db3a807b197
WalidurTanjim <mohammadtanjim91@gmail.com>
2025-04-26 07:40:12 +0600

    Array of TS

---
+++ array.ts
@@ -0,0 +1,16 @@
+// In TS, we can create & init arrays using various approaches.
+
+// 01: Using square brackets
+// 02: Using array constructors
+// 03: Using theArray.of method
+
+// Ex-01:
+const numsArray: number[] = [1,2,3,4,5];
+
+// Ex-02:
+const numsArray1: number[] = new Array(1, 2, 3, 4, 5);
+
+// Ex-03:
+const namesArray: string[] = Array.of('Walid', 'Tanjim');
+
+// console.log(numsArray, numsArray1, namesArray[1]);
